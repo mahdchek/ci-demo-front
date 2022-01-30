f11b75e8af3239fbc8aa12faa337181a196a1a2b
mahdi chekini <me.chekini@gmail.com>
2022-01-30 15:18:31 +0100

    init frontend project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from './core/services/user.service';
+import {UserModel} from './core/models/user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ci-demo-front';
+  users: UserModel[] = [];
+  constructor(private userService: UserService) {
+  }
+  ngOnInit(): void {
+    console.log('coucou les amis saleu');
+    this.userService.getUsers().subscribe( data => {
+      this.users = data;
+    });
+  }
 }
---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,7 @@
+export interface UserModel {
+  id: number;
+  nom: string;
+  prenom: string;
+  login: string;
+  role: string;
+}
